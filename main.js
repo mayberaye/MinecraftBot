ae21a6eddff81d8a811eef7d8bab5ba2eba2d9d5
Raye Nascence <rayemayberry@yahoo.com>
2022-12-03 21:04:32 -0600

    successfully spawn bot in world

---
+++ main.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer')
+
+const options = {
+  host: 'localhost', // Change this to the ip you want.
+  port: 44387 , // Change this to the port you want.
+  username: 'MinorlyAutomated',
+}
+
+const bot = mineflayer.createBot(options)
